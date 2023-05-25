92d4485caceb6131e6f5f4438b6f2187ed4247e9
Edin Ostrakovic <edo.ostrakovic@gmail.com>
2023-05-25 21:39:27 +0200

    Initial Commit

---
+++ UberSwiftUIDemo/UberSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIDemoApp.swift
+//  UberSwiftUIDemo
+//
+//  Created by home on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
